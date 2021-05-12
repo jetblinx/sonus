6f29ea4b05a08dc3cb0fbd6eb3bcab75385366df
Deliveos <deliveosdars@gmail.com>
2021-05-13 00:12:18 +0600

    Maked settings screen

--- lib/ui/screens/settings/components/body.dart
+++ lib/ui/screens/settings/components/body.dart
@@ -5,7 +5,9 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(children: [
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
         SizedBox(
           height: 50,
         ),
@@ -13,12 +15,67 @@ class Body extends StatelessWidget {
           padding: EdgeInsets.symmetric(horizontal: 30),
           child: Text(
             AppLocalizations.of(context).settings,
-            style: TextStyle(
-                color: Colors.black87,
-                fontWeight: FontWeight.w600,
-                fontSize: 24),
+            style: Theme.of(context).textTheme.caption
           ),
         ),
+        SizedBox(
+          height: 50,
+        ),
+        
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(AppLocalizations.of(context).modules, style: Theme.of(context).textTheme.headline1),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(AppLocalizations.of(context).speech_recognition, style: Theme.of(context).textTheme.bodyText1),
+                  Switch(
+                    activeColor: Theme.of(context).backgroundColor,
+                    activeTrackColor: Theme.of(context).primaryColor,
+                    value: true, 
+                    onChanged: (bool value) {}
+                  )
+                ]
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(AppLocalizations.of(context).speech_to_text, style: Theme.of(context).textTheme.bodyText1),
+                  Switch(
+                    activeColor: Theme.of(context).backgroundColor,
+                    activeTrackColor: Theme.of(context).primaryColor,
+                    value: true, 
+                    onChanged: (bool value) {}
+                  )
+                ]
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Divider(
+                color: Colors.grey,
+                height: 1,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(AppLocalizations.of(context).language, style: Theme.of(context).textTheme.bodyText1),
+                  Text(AppLocalizations.of(context).russian, style: Theme.of(context).textTheme.bodyText1)
+                ]
+              ),
+            ]
+          ),
+        )
+        
       ]),
       // appBar: AppBar(
       //   centerTitle: true,
