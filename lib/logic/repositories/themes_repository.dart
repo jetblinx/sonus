df2f7c57579d34103a165e40040f5862088a0b82
Deliveos <deliveosdars@gmail.com>
2021-05-20 23:31:43 +0600

    Update font size

--- lib/logic/repositories/themes_repository.dart
+++ lib/logic/repositories/themes_repository.dart
@@ -11,29 +11,72 @@ class ThemesRepisotory {
       backgroundColor: lightScaffoldBackgroundColor,
       textTheme: TextTheme(
         bodyText1: TextStyle(
+          // fontFamily: "Open Sans",
           color: lightAccentColor,
-          fontSize: 18.0
+          fontSize: 16.0
         ),
-        bodyText2: TextStyle(color: lightAccentColor, fontSize: 16.0),
-        button: TextStyle(color: lightAccentColor, fontSize: 16.0),
-        subtitle1: TextStyle(color: lightAccentColor, fontSize: 16.0),
-        subtitle2: TextStyle(color: lightAccentColor, fontSize: 16.0),
+        bodyText2: TextStyle(
+          fontFamily: "Open Sans",
+          color: lightAccentColor, 
+          fontSize: 14.0
+        ),
+        button: TextStyle(
+          fontFamily: "Open Sans",
+          color: lightAccentColor, 
+          fontSize: 16.0
+        ),
+        subtitle1: TextStyle(
+          fontFamily: "Open Sans",
+          color: lightAccentColor, 
+          fontSize: 16.0
+        ),
+        subtitle2: TextStyle(
+          fontFamily: "Open Sans",
+          color: lightAccentColor, 
+          fontSize: 16.0
+        ),
         caption: TextStyle(
+          fontFamily: "Open Sans",
           color: lightAccentColor,
           fontWeight: FontWeight.w600,
-          fontSize: 24.0
+          fontSize: 18.0
         ),
         headline1: TextStyle(
+          fontFamily: "Open Sans",
           color: lightAccentColor,
           fontWeight: FontWeight.w800,
-          fontSize: 24.0
+          fontSize: 16.0
         ),
-        headline2: TextStyle(color: lightAccentColor, fontSize: 16.0),
-        headline3: TextStyle(color: lightAccentColor, fontSize: 16.0),
-        headline4: TextStyle(color: lightAccentColor, fontSize: 16.0),
-        headline5: TextStyle(color: lightAccentColor, fontSize: 16.0),
-        headline6: TextStyle(color: lightAccentColor, fontSize: 16.0),
-        overline: TextStyle(color: lightAccentColor, fontSize: 16.0),
+        headline2: TextStyle(
+          fontFamily: "Open Sans",
+          color: lightAccentColor, 
+          fontSize: 16.0
+        ),
+        headline3: TextStyle(
+          fontFamily: "Open Sans",
+          color: lightAccentColor, 
+          fontSize: 16.0
+        ),
+        headline4: TextStyle(
+          fontFamily: "Open Sans",
+          color: lightAccentColor, 
+          fontSize: 16.0
+        ),
+        headline5: TextStyle(
+          fontFamily: "Open Sans",
+          color: lightAccentColor, 
+          fontSize: 16.0
+        ),
+        headline6: TextStyle(
+          fontFamily: "Open Sans",
+          color: lightAccentColor, 
+          fontSize: 16.0
+        ),
+        overline: TextStyle(
+          fontFamily: "Open Sans",
+          color: lightAccentColor, 
+          fontSize: 16.0
+        ),
       ),
   );
 
@@ -44,27 +87,73 @@ class ThemesRepisotory {
     buttonColor: darkAccentColor,
     backgroundColor: darkScaffoldBackgroundColor,
     textTheme: TextTheme(
-      bodyText1: TextStyle(color: darkAccentColor, fontSize: 18.0),
-      bodyText2: TextStyle(color: darkAccentColor, fontSize: 16.0),
-      button: TextStyle(color: darkAccentColor, fontSize: 16.0),
-      subtitle1: TextStyle(color: darkAccentColor, fontSize: 16.0),
-      subtitle2: TextStyle(color: darkAccentColor, fontSize: 16.0),
+      bodyText1: TextStyle(
+        // fontFamily: "Open Sans",
+        color: darkAccentColor, 
+        fontSize: 14.0
+      ),
+      bodyText2: TextStyle(
+        // fontFamily: "Open Sans",
+        color: darkAccentColor, 
+        fontSize: 14.0
+      ),
+      button: TextStyle(
+        // fontFamily: "Open Sans",
+        color: darkAccentColor, 
+        fontSize: 16.0
+      ),
+      subtitle1: TextStyle(
+        // fontFamily: "Open Sans",
+        color: darkAccentColor, 
+        fontSize: 16.0
+      ),
+      subtitle2: TextStyle(
+        // fontFamily: "Open Sans",
+        color: darkAccentColor, 
+        fontSize: 16.0
+      ),
       caption: TextStyle(
+        // fontFamily: "Open Sans",
         color: darkTextColor,
         fontWeight: FontWeight.w600,
-        fontSize: 24.0
+        fontSize: 18.0
       ),
       headline1: TextStyle(
+        // fontFamily: "Open Sans",
         color: darkAccentColor,
         fontWeight: FontWeight.w800,
-        fontSize: 24.0
+        fontSize: 16.0
       ),
-      headline2: TextStyle(color: darkAccentColor, fontSize: 16.0),
-      headline3: TextStyle(color: darkAccentColor, fontSize: 16.0),
-      headline4: TextStyle(color: darkAccentColor, fontSize: 16.0),
-      headline5: TextStyle(color: darkAccentColor, fontSize: 16.0),
-      headline6: TextStyle(color: darkAccentColor, fontSize: 16.0),
-      overline: TextStyle(color: darkAccentColor, fontSize: 16.0),
+      headline2: TextStyle(
+        // fontFamily: "Open Sans",
+        color: darkAccentColor, 
+        fontSize: 16.0
+      ),
+      headline3: TextStyle(
+        // fontFamily: "Open Sans",
+        color: darkAccentColor, 
+        fontSize: 16.0
+      ),
+      headline4: TextStyle(
+        // fontFamily: "Open Sans",
+        color: darkAccentColor, 
+        fontSize: 16.0
+      ),
+      headline5: TextStyle(
+        // fontFamily: "Open Sans",
+        color: darkAccentColor, 
+        fontSize: 16.0
+      ),
+      headline6: TextStyle(
+        // fontFamily: "Open Sans",
+        color: darkAccentColor, 
+        fontSize: 16.0
+      ),
+      overline: TextStyle(
+        // fontFamily: "Open Sans",
+        color: darkAccentColor, 
+        fontSize: 16.0
+      ),
     )
   );
 
