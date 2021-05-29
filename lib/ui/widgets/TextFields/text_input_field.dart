4c64e5dc0f14bcfca80a85bf7bf536f20869191e
commanderxa <art.ostrikov@gmail.com>
2021-05-29 14:19:24 +0600

    Merge branch 'main' of https://github.com/CommanderXA/sonus

--- lib/ui/widgets/TextFields/text_input_field.dart
+++ lib/ui/widgets/TextFields/text_input_field.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sonus/logic/repositories/TextField/text_field.dart';
 import 'package:sonus/utils/constants.dart';
 import 'package:sonus/utils/icons.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -12,15 +11,16 @@ class TextInputField extends StatelessWidget {
     this.backgroundTransaprent = false,
     this.centerAlign = false,
     this.maxLines = kSizeTextFieldLines,
-  })  :  super(key: key);
+    this.onChanged,
+  }) : super(key: key);
+
+  final Function onChanged;
 
   final Icon icon;
   final bool isBorder;
   final bool backgroundTransaprent;
   final bool centerAlign;
   final int maxLines;
-
-  final TextBloc _textBloc = TextBloc();
 
   @override
   Widget build(BuildContext context) {
@@ -37,32 +37,29 @@ class TextInputField extends StatelessWidget {
       ),
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: kPaddingAllHorizontal),
-        child: StreamBuilder(
-            stream: _textBloc.textStream,
-            builder: (ctxt, AsyncSnapshot<String> textStream) {
-              return Form(
+        child: Form(
                 child: TextFormField(
                   style: Theme.of(context).textTheme.headline3,
                   textAlign: centerAlign ? TextAlign.center : TextAlign.start,
                   textCapitalization: TextCapitalization.sentences,
-                  onChanged: (String text) => _textBloc.updateText(text),
+                  onChanged: onChanged,
                   cursorColor: Colors.black,
                   decoration: InputDecoration(
                       border: InputBorder.none,
                       hintStyle: Theme.of(context).textTheme.headline3,
                       hintText: AppLocalizations.of(context).enter_text,
-                      icon: textStream.hasError ? null : Icon(kIconDismiss),
-                      suffixIcon: icon != null
+                      icon: icon == null ? null : Icon(kIconDismiss),
+                      suffixIcon: true
                           ? IconButton(
                               icon: icon,
                               onPressed: () {},
                             )
-                          : null),
+                          : null
+                      ),
                   minLines: 1,
                   maxLines: maxLines != null ? kSizeTextFieldLines : null,
                 ),
-              );
-            }),
+              ),
       ),
     );
   }
