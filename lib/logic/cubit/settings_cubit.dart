3fa5c3803a048c82ba66770a33df2b8ee3960852
Deliveos <deliveosdars@gmail.com>
2021-05-29 12:15:37 +0600

    fixed text-to-speech display

--- lib/logic/cubit/settings_cubit.dart
+++ lib/logic/cubit/settings_cubit.dart
@@ -2,7 +2,6 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:sonus/logic/models/settings_model.dart';
 import 'package:sonus/logic/repositories/settings_repository.dart';
-import 'package:sonus/utils/logger.dart';
 
 part 'settings_state.dart';
 
@@ -15,13 +14,7 @@ class SettingsCubit extends Cubit<SettingsState> {
 
   Future<void> load() async {
     try {
-    //   if (state is SettingsLoadedState) {
-    //     emit(SettingsPrevState(state: state));
-    //   } else {
-    //     emit(SettingsInitialState());
-    //   }
       final settings = await _repository.settings;
-      Logger.log(settings.toString());
       emit(SettingsLoadedState(settings));
     } catch (e) {
       print(e);
@@ -30,10 +23,8 @@ class SettingsCubit extends Cubit<SettingsState> {
 
   Future<void> update(SettingsModel settings) async {
     final SettingsLoadedState currentState = state;
-    Logger.log(settings.toString());
     emit(SettingsInitialState());
     emit(currentState.copyWith(settings));
     await _repository.update(currentState.settings.copyWith(settings));
-    // load();s
   }
 }
