7431f0bfee838e5bf39ce70f888fe2cbf482d243
commanderxa <art.ostrikov@gmail.com>
2021-06-05 11:58:51 +0600

    Created some structure

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_fr.dart
@@ -0,0 +1,165 @@
+
+
+
+import 'app_localizations.dart';
+
+/// The translations for French (`fr`).
+class AppLocalizationsFr extends AppLocalizations {
+  AppLocalizationsFr([String locale = 'fr']) : super(locale);
+
+  @override
+  String get hello => 'Bonjour';
+
+  @override
+  String get continue_forward => 'Continuer';
+
+  @override
+  String get short_description => 'Outil de reconnaissance et de synthèse vocale';
+
+  @override
+  String get sonus => 'SONUS';
+
+  @override
+  String get accept => 'Accept';
+
+  @override
+  String get add_phrase => 'Ajouter une phrase';
+
+  @override
+  String get add_records_group => 'Ajouter un groupe d\'enregistrements';
+
+  @override
+  String get archive_of_records => 'Archive des enregistrements';
+
+  @override
+  String get editing_phrases => 'Phrases rapides';
+
+  @override
+  String get edit_phrase => 'Modifier la phrase';
+
+  @override
+  String get edit_records_group => 'Modifier le groupe d\'enregistrements';
+
+  @override
+  String get english => 'Anglais';
+
+  @override
+  String get enter_text => 'Entrez du texte';
+
+  @override
+  String get language => 'Language';
+
+  @override
+  String get modules => 'Modules';
+
+  @override
+  String get language_choosen => 'Français';
+
+  @override
+  String get no_phrases => 'Pas de prases rapides';
+
+  @override
+  String get no_records_saved => 'Aucun enregistrement enregistré';
+
+  @override
+  String get no_records => 'Aucun enregistrement';
+
+  @override
+  String get settings => 'Paramètres';
+
+  @override
+  String get speech_recognition => 'Reconnaissance vocale';
+
+  @override
+  String get text_to_speech => 'Texte en parole';
+
+  @override
+  String get system_language => 'Système';
+
+  @override
+  String get dark_theme => 'Thème sombre';
+
+  @override
+  String get settings_general => 'Général';
+
+  @override
+  String get quick_tts => 'TTS rapide';
+
+  @override
+  String get notes => 'Notes';
+
+  @override
+  String get note_groups => 'Groupes de notes';
+
+  @override
+  String get record_name => 'Nom de l\'enregistrement';
+
+  @override
+  String get record_save => 'Enregistrer';
+
+  @override
+  String get close => 'Fermer';
+
+  @override
+  String get choose_group => 'Choisissez un groupe';
+
+  @override
+  String get record_added => 'Enregistrement ajouté';
+
+  @override
+  String get record_edited => 'Enregistrement édité';
+
+  @override
+  String get record_deleted => 'Enregistrement supprimé';
+
+  @override
+  String get quick_phrase_added => 'Expression rapide ajoutée';
+
+  @override
+  String get quick_phrase_edited => 'Expression rapide modifiée';
+
+  @override
+  String get quick_phrase_deleted => 'Expression rapide supprimée';
+
+  @override
+  String get record_group_added => 'Groupe d\'enregistrements ajouté';
+
+  @override
+  String get record_group_edited => 'Groupe d\'enregistrements modifié';
+
+  @override
+  String get record_group_deleted => 'Groupe d\'enregistrements supprimé';
+
+  @override
+  String get phrase_copied => 'Phrase copiée';
+
+  @override
+  String get no_internet_connection => 'Pas de connexion Internet';
+
+  @override
+  String get no_mic_permission => 'La reconnaissance vocale nécessite l\'utilisation d\'un microphone';
+
+  @override
+  String get grant_mic_permission => 'Appuyez sur l\'icône pour accorder l\'autorisation';
+
+  @override
+  String get help => 'Aider';
+
+  @override
+  String get help_stt_not_work => 'La reconnaissance vocale ne fonctionne pas';
+
+  @override
+  String get help_stt_not_work_solution => 'La reconnaissance vocale utilise les services Google pour reconnaître votre parole. Si cela ne fonctionne pas, recherchez l\'application Google dans les paramètres et accordez-lui une autorisation de microphone.';
+
+  @override
+  String get help_tts_not_work => 'La synthèse vocale ne fonctionne pas';
+
+  @override
+  String get help_tts_not_work_solution => 'Speech to text utilise le plug-in Google Speech-to-text pour la synthèse vocale. Cela peut ne pas fonctionner si vous n\'avez pas installé ce plugin. Accédez à Google Play et recherchez Google Text-to-Speech, puis installez-le et redémarrez SONUS (fermez et ouvrez à nouveau).';
+
+  @override
+  String get privacy_policy => 'Privacy Policy';
+
+  @override
+  String get terms_of_service => 'Terms Of Service';
+}
