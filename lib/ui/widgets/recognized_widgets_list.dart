00553ccddbc5ca67682e32faa930900c43be7e11
commanderxa <art.ostrikov@gmail.com>
2021-06-06 15:08:14 +0600

    Added ability to delete phrase on Speech Recognition Screen

--- lib/ui/widgets/recognized_widgets_list.dart
+++ lib/ui/widgets/recognized_widgets_list.dart
@@ -4,7 +4,7 @@ import 'package:sonus/ui/widgets/snackbars/snackbar.dart';
 import 'package:sonus/utils/icons.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class RecognizedPhrasesList extends StatelessWidget {
+class RecognizedPhrasesList extends StatefulWidget {
   const RecognizedPhrasesList({
     Key key,
     ScrollController scrollController,
@@ -18,16 +18,134 @@ class RecognizedPhrasesList extends StatelessWidget {
   final List<String> archiveSpeech;
 
   @override
+  _RecognizedPhrasesListState createState() => _RecognizedPhrasesListState();
+}
+
+class _RecognizedPhrasesListState extends State<RecognizedPhrasesList> {
+  Container _buildModelBottomSheetMenu(int index, bool archive) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 20),
+          Center(
+            child: Container(
+              width: 30,
+              height: 5,
+              decoration: BoxDecoration(
+                  color: Theme.of(context).accentColor,
+                  borderRadius: BorderRadius.circular(50)),
+            ),
+          ),
+          ListTile(
+            title: Row(
+              children: [
+                Icon(
+                  kIconCopy,
+                  color: Theme.of(context).accentColor,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  AppLocalizations.of(context).copy,
+                  style: Theme.of(context).textTheme.headline3,
+                ),
+              ],
+            ),
+            onTap: () {
+              Clipboard.setData(new ClipboardData(
+                      text: archive
+                          ? widget.archiveSpeech[index]
+                          : widget.speechRecognized[index]))
+                  .then((_) {
+                Navigator.pop(context);
+                final snackBar = FloatingSnackbar.floatingSnackBar(
+                    Icon(
+                      kIconCopy,
+                      color: Theme.of(context).accentColor,
+                    ),
+                    AppLocalizations.of(context).phrase_copied,
+                    AppLocalizations.of(context).close,
+                    context);
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              });
+            },
+          ),
+          Divider(),
+          ListTile(
+            title: Row(
+              children: [
+                Icon(
+                  kIconDelete,
+                  color: Theme.of(context).accentColor,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  AppLocalizations.of(context).delete,
+                  style: Theme.of(context).textTheme.headline3,
+                ),
+              ],
+            ),
+            onTap: () {
+              setState(() => {
+                    archive
+                        ? widget.archiveSpeech.removeAt(index)
+                        : widget.speechRecognized.removeAt(index)
+                  });
+              
+              Navigator.pop(context);
+              final snackBar = FloatingSnackbar.floatingSnackBar(
+                  Icon(
+                    kIconDelete,
+                    color: Theme.of(context).accentColor,
+                  ),
+                  AppLocalizations.of(context).phrase_deleted,
+                  AppLocalizations.of(context).close,
+                  context);
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _modalBottomSheetMenu(int index, bool archive) {
+    showModalBottomSheet(
+        backgroundColor: Colors.transparent,
+        context: context,
+        builder: (builder) {
+          return Container(
+            height: 180,
+            decoration: BoxDecoration(
+              color: Theme.of(context).backgroundColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
+            ),
+            child: _buildModelBottomSheetMenu(index, archive),
+          );
+        });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return archiveSpeech == null
+    return widget.archiveSpeech == null
         ? ListView.separated(
-            controller: _scrollController,
-            itemCount: speechRecognized.length,
+            controller: widget._scrollController,
+            itemCount: widget.speechRecognized.length,
             itemBuilder: (BuildContext context, int index) {
               return ListTile(
+                onLongPress: () {
+                  _modalBottomSheetMenu(index, false);
+                },
                 onTap: () {
-                  Clipboard.setData(
-                          new ClipboardData(text: speechRecognized[index]))
+                  Clipboard.setData(new ClipboardData(
+                          text: widget.speechRecognized[index]))
                       .then((_) {
                     final snackBar = FloatingSnackbar.floatingSnackBar(
                         Icon(
@@ -41,7 +159,7 @@ class RecognizedPhrasesList extends StatelessWidget {
                   });
                 },
                 title: Text(
-                  speechRecognized[index],
+                  widget.speechRecognized[index],
                   style: Theme.of(context).textTheme.caption,
                   textAlign: TextAlign.start,
                 ),
@@ -52,12 +170,12 @@ class RecognizedPhrasesList extends StatelessWidget {
             },
           )
         : ListView.separated(
-            itemCount: archiveSpeech.length,
+            itemCount: widget.archiveSpeech.length,
             itemBuilder: (BuildContext context, int index) {
               return ListTile(
                 onTap: () {
                   Clipboard.setData(
-                          new ClipboardData(text: archiveSpeech[index]))
+                          new ClipboardData(text: widget.archiveSpeech[index]))
                       .then((_) {
                     final snackBar = FloatingSnackbar.floatingSnackBar(
                         Icon(
@@ -71,7 +189,7 @@ class RecognizedPhrasesList extends StatelessWidget {
                   });
                 },
                 title: Text(
-                  archiveSpeech[index],
+                  widget.archiveSpeech[index],
                   style: Theme.of(context).textTheme.caption,
                   textAlign: TextAlign.start,
                 ),
